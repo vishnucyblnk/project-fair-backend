29aef5f4cc40fe4973865ef43ca3fdf167469a6c
VISHNU NAIR B <0880vishnu@gmail.com>
2024-06-08 17:52:37 +0530

    secret

--- Middlewares/jwtMiddleware.js
+++ Middlewares/jwtMiddleware.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const jwtMiddleware = (req,res,next)=>{
     console.log("Inside jwtMiddleware");
     const token = req.headers['authorization'].split(" ")[1]
     try{
-        const jwtResponse = jwt.verify(token,"superSecretKey123")
+        const jwtResponse = jwt.verify(token,process.env.SECRET_KEY)
         console.log(jwtResponse);
         req.payload = jwtResponse.userId
         next()
