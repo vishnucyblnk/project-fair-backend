a522e75974f44205aab9536f9392e1e5d198c7fd
VISHNU NAIR B <0880vishnu@gmail.com>
2024-08-19 20:16:29 +0530

    somemore-multer-changes

--- Middlewares/multerMiddleware.js
+++ Middlewares/multerMiddleware.js
@@ -1,18 +1,18 @@
 const multer = require('multer');
 
-const storage = multer.diskStorage({
+const imageStorage = multer.diskStorage({
     destination:(req,file,callback)=>{
         callback(null,'./uploads/images')
     },
-    filename:(req,file,callback)=>{
+    filename: (req,file,callback)=>{
         const filename= `Image-${Date.now()}-${file.originalname}`
         callback(null,filename)
     }
 })
 
-const fileFilter = (req,file,callback)=>{
-    if(file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg" ){
-        callback(null,true)
+const imageFileFilter = (req,file,callback)=>{
+    if(file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimmetype === "image/jpg"){
+        callback(null,true);
     }else{
         callback(null,false)
         return callback(new Error("Only .png, .jpg , .jpeg file are allowed!!!"))
